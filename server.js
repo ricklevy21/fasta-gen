ea67b1cd5e679d979a0c2343de714efafd805bf9
Rick Levy <ricklevy21@gmail.com>
2020-09-23 07:02:38 -0600

    started setting up server

---
+++ server.js
@@ -0,0 +1,14 @@
+//dependencies
+var express = require("express");
+var path = require("path");
+
+
+//create the server
+var app = express();
+
+//define the PORT
+var PORT = process.env.PORT || 8080;
+
+
+
+

e0ee87602904e96e8ffd0dcad49cf1a18316dcba
Rick Levy <Richard.Levy@BotanicGardens.org>
2020-10-27 12:25:41 -0600

    server.js organized better. handlebars set up and home page route being served correctly.

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
+//dependencies
 const express = require("express");
-const app = express();
-const db = require("./models");
-const initRoutes = require("./routes/api-routes");
+const exphbs = require("express-handlebars");
 
+// require models for syncing
+const db = require("./models");
+
+
+//create express server
+const app = express();
+
+//define PORT where server will listen for requests (production || developement)
+var PORT = process.env.PORT || 8080;
+
+//set the global root dir
 global.__basedir = __dirname + "/.";
 
+//set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// define static directory
+app.use(express.static("public"));
+
+// setup handelbars
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//routes
+const initRoutes = require("./routes/api-routes");
+//api routes for handling data exchanges
 initRoutes(app);
+require("./routes/view-routes.js")(app);
 
-db.sequelize.sync();
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-let port = 8080;
-app.listen(port, () => {
-  console.log(`Running at localhost:${port}`);
+// start the server. {force:true} drops the tables from exisiting db. {force:false} keeps the existing db and tables and data in place
+db.sequelize.sync({
+  force: false
+}).then(function () {
+  app.listen(PORT, function () {
+      console.log("App listening at http://localhost:" + PORT);
+  });
 });

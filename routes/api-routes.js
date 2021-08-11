018f5f1248a48186466be917fa2ae741a1c07d81
Rick Levy <Richard.Levy@BotanicGardens.org>
2021-08-11 08:04:08 -0600

    modified baseURL in routes for auth

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -46,8 +46,8 @@ let routes = (app) => {
       const config = {
         authRequired: false,
         auth0Logout: true,
-        secret: 'process.env.AUTH0_SECRET',
-        baseURL: 'http://localhost:8080',
+        secret: process.env.AUTH0_SECRET,
+        baseURL: process.env.AUTH0_BASEURL,
         clientID: process.env.AUTH0_ClientID,
         issuerBaseURL: 'https://dev-bh957c52.us.auth0.com'
     };

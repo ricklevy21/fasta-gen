21c33fd5f05dc4caa1a26a3844729326243951a9
Rick Levy <Richard.Levy@BotanicGardens.org>
2022-03-25 11:21:59 -0600

    removed port from config

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,6 @@ module.exports = {
   USER: process.env.DB_DEV_USER,
   PASSWORD: process.env.DB_DEV_PASSWORD,
   DB: process.env.DB_DEV_DATABASE,
-  PORT: process.env.PORT,
   dialect: "mysql",
   pool: {
     max: 5,

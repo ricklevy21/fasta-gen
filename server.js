8010e50bee8cc0580464ceff7ddd0a77bd27b856
Rick Levy <Richard.Levy@BotanicGardens.org>
2022-10-12 15:14:56 -0600

    update

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const db = require("./models");
 const app = express();
 
 //define PORT where server will listen for requests (production || developement)
-//var PORT = process.env.PORT || 8080;
-var PORT = 8080
+var PORT = process.env.PORT || 8080;
+//var PORT = 8080
 
 //set the global root dir
 global.__basedir = __dirname + "/.";
